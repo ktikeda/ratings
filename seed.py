26d157a18d4e653e42051ff54a9a8da20d433f83
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-30 13:06:51 -0700

    Working on load_movies()

--- seed.py
+++ seed.py
@@ -9,6 +9,7 @@ from model import connect_to_db, db
 from server import app
 
 import datetime
+import re
 
 
 def load_users():
@@ -21,16 +22,16 @@ def load_users():
     User.query.delete()
 
     # Read u.user file and insert data
-    for row in open("seed_data/u.user"):
-        row = row.rstrip()
-        user_id, age, gender, occupation, zipcode = row.split("|")
+    with open("seed_data/u.user") as user_data: 
 
-        user = User(user_id=user_id,
-                    age=age,
-                    zipcode=zipcode)
-
-        # We need to add to the session or it won't ever be stored
-        db.session.add(user)
+        for row in user_data:
+            row = row.rstrip()
+            user_id, age, gender, occupation, zipcode = row.split("|")
+            user = User(user_id=user_id,
+                        age=age,
+                        zipcode=zipcode)
+            # We need to add to the session or it won't ever be stored
+            db.session.add(user)
 
     # Once we're done, we should commit our work
     db.session.commit()
@@ -38,6 +39,20 @@ def load_users():
 
 def load_movies():
     """Load movies from u.item into database."""
+
+    print "Movies"
+
+    with open("seed_data/u.item") as movie_data:
+
+
+        id = 1
+        for row in movie_data:
+            row = row.rstrip()
+            row_data = row.split("|")
+            movie_id = id
+            id += 1
+            title = re.match()
+
 
 
 def load_ratings():
